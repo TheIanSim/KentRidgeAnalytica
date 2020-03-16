1d54bb1980ce572d54b79b87849aeedeabc95650
Ian Sim <e0031628@u.nus.edu>
2020-03-16 15:28:15 +0800

    add test

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 from classes import Graph, Node, Simulation
 
 test_sim = Simulation()
-
-t = test_sim.run_one_timestep()
-
-print(next(t))
-
-print(next(t))
-
-
-print(next(t))
-
-print(next(t))
-
